2133793e1907be391a6e56d249dfe7a13a1a25bf
wupa9 <wupaz9@gmail.com>
2017-03-10 02:50:07 -0600

    Compute and cache common metadata

--- ext2/init.cpp
+++ ext2/init.cpp
@@ -1,6 +1,23 @@
 #include "operations.h"
+#include "super.h"
+#include "inode.h"
+#include "groupdesc.h"
+#include "blockgroup.h"
+#include "../device/device.h"
+
 #include <stdio.h>
 
 void* init(struct fuse_conn_info *conn) {
     printf("OPERATION: init...\n");
+
+    Ext2SuperBlock sb;
+
+    if (!readSuperBlock(&sb))
+        return 0;
+
+    blockSize = 1024 << sb.s_log_block_size;
+    groupCount = 1 + (sb.s_blocks_count - 1) / sb.s_blocks_per_group;
+    descriptorListSize = groupCount * GD_SIZE;
+    inodesPerBlock = blockSize / INODE_SIZE;
+    itableBlockCount = sb.s_inodes_per_group / inodesPerBlock;
 }
