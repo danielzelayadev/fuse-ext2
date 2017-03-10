256444e563b9d03c1635cf7358b3faa738ac8966
wupa9 <wupaz9@gmail.com>
2017-03-10 03:56:18 -0600

    Add utils and split string function

---
+++ ext2/utils.cpp
@@ -0,0 +1,17 @@
+#include "utils.h"
+#include <iostream>
+
+void split(const string& s, char c, vector<string>& v) {
+   string::size_type i = 0;
+   string::size_type j = s.find(c);
+
+   while (j != string::npos) {
+      if (i != j)
+            v.push_back(s.substr(i, j-i));
+      i = ++j;
+      j = s.find(c, j);
+
+      if (j == string::npos)
+         v.push_back(s.substr(i, s.length()));
+   }
+}
---
+++ ext2/utils.h
@@ -0,0 +1,11 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <vector>
+#include <string>
+
+using namespace std;
+
+void split(const string& s, char c, vector<string>& v);
+
+#endif
