8283bb5bcd5541fa37ebd6bec70080bab5b9dbac
wupa9 <wupaz9@gmail.com>
2017-03-10 02:48:56 -0600

    Add structs for inodes, descriptors, and dentries

--- ext2/types.h
+++ ext2/types.h
@@ -43,12 +43,91 @@ struct Ext2SuperBlock {
 	 * things it doesn't understand...
 	 */
 	uint32_t	s_first_ino; 		/* First non-reserved inode */
-	uint16_t   s_inode_size; 		/* size of inode structure */
+	uint16_t    s_inode_size; 		/* size of inode structure */
 	uint16_t	s_block_group_nr; 	/* block group # of this superblock */
 	uint32_t	s_feature_compat; 	/* compatible feature set */
 	uint32_t	s_feature_incompat; 	/* incompatible feature set */
 	uint32_t	s_feature_ro_compat; 	/* readonly-compatible feature set */
 	uint32_t	s_reserved[230];	/* Padding to the end of the block */
+};
+
+#define	EXT2_NDIR_BLOCKS		12
+#define	EXT2_IND_BLOCK			EXT2_NDIR_BLOCKS
+#define	EXT2_DIND_BLOCK			(EXT2_IND_BLOCK + 1)
+#define	EXT2_TIND_BLOCK			(EXT2_DIND_BLOCK + 1)
+#define	EXT2_N_BLOCKS			(EXT2_TIND_BLOCK + 1)
+
+struct Ext2Inode {
+	uint16_t	i_mode;		/* File mode */
+	uint16_t	i_uid;		/* Owner Uid */
+	uint32_t	i_size;		/* Size in bytes */
+	uint32_t	i_atime;	/* Access time */
+	uint32_t	i_ctime;	/* Creation time */
+	uint32_t	i_mtime;	/* Modification time */
+	uint32_t	i_dtime;	/* Deletion Time */
+	uint16_t	i_gid;		/* Group Id */
+	uint16_t	i_links_count;	/* Links count */
+	uint32_t	i_blocks;	/* Blocks count */
+	uint32_t	i_flags;	/* File flags */
+	union {
+		struct {
+			uint32_t  l_i_reserved1;
+		} linux1;
+		struct {
+			uint32_t  h_i_translator;
+		} hurd1;
+		struct {
+			uint32_t  m_i_reserved1;
+		} masix1;
+	} osd1;				/* OS dependent 1 */
+
+	uint32_t	i_block[EXT2_N_BLOCKS];/* Pointers to blocks */
+	uint32_t	i_version;	/* File version (for NFS) */
+	uint32_t	i_file_acl;	/* File ACL */
+	uint32_t	i_dir_acl;	/* Directory ACL */
+	uint32_t	i_faddr;	/* Fragment address */
+	union {
+		struct {
+			uint8_t	l_i_frag;	/* Fragment number */
+			uint8_t	l_i_fsize;	/* Fragment size */
+			uint16_t	i_pad1;
+			uint32_t	l_i_reserved2[2];
+		} linux2;
+		struct {
+			uint8_t	h_i_frag;	/* Fragment number */
+			uint8_t	h_i_fsize;	/* Fragment size */
+			uint16_t	h_i_mode_high;
+			uint16_t	h_i_uid_high;
+			uint16_t	h_i_gid_high;
+			uint32_t	h_i_author;
+		} hurd2;
+		struct {
+			uint8_t	m_i_frag;	/* Fragment number */
+			uint8_t	m_i_fsize;	/* Fragment size */
+			uint16_t	m_pad1;
+			uint32_t	m_i_reserved2[2];
+		} masix2;
+	} osd2;				/* OS dependent 2 */
+};
+
+struct Ext2GroupDescriptor {
+	uint32_t	bg_block_bitmap;		/* Blocks bitmap block */
+	uint32_t	bg_inode_bitmap;		/* Inodes bitmap block */
+	uint32_t	bg_inode_table;		/* Inodes table block */
+	uint16_t	bg_free_blocks_count;	/* Free blocks count */
+	uint16_t	bg_free_inodes_count;	/* Free inodes count */
+	uint16_t	bg_used_dirs_count;	/* Directories count */
+	uint16_t	bg_pad;
+	uint32_t	bg_reserved[3];
+};
+
+#define EXT2_NAME_LEN 255
+
+struct Ext2DirEntry {
+	uint32_t	inode;			/* Inode number */
+	uint16_t	rec_len;		/* Directory entry length */
+	uint16_t	name_len;		/* Name length */
+	char	name[EXT2_NAME_LEN];	/* File name */
 };
 
 #endif
