8a60fd08364b2006d1f31071558f8d020d35e4c0
wupa9 <wupaz9@gmail.com>
2017-03-10 19:05:35 -0600

    getattr done

--- ext2/getattr.cpp
+++ ext2/getattr.cpp
@@ -1,7 +1,32 @@
 #include "operations.h"
+#include "inode.h"
+#include "types.h"
+#include <errno.h>
 #include <stdio.h>
+#include <string>
+
+using namespace std;
 
 int getattr(const char *path, struct stat *statbuf) {
     printf("OPERATION: getattr(path=%s)\n", path);
-    return 1;
+
+    Ext2Inode inode;
+    string p(path);
+
+    if (!getInodeByPath(p, &inode))
+        return -ENOENT;
+    
+    // printInode(inode);
+
+    statbuf->st_mode   = inode.i_mode;
+    statbuf->st_nlink  = inode.i_links_count;
+    statbuf->st_size   = inode.i_size;
+    statbuf->st_blocks = inode.i_blocks;
+    statbuf->st_uid    = inode.i_uid;
+    statbuf->st_gid    = inode.i_gid;
+    statbuf->st_atime  = inode.i_atime;
+    statbuf->st_mtime  = inode.i_mtime;
+    statbuf->st_ctime  = inode.i_ctime;
+
+    return 0;
 }
