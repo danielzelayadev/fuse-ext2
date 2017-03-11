2562503231521310f1f4fd296d9c85a9490e80ab
wupa9 <wupaz9@gmail.com>
2017-03-11 03:52:30 -0600

    Refactor readDentry

--- ext2/dentry.cpp
+++ ext2/dentry.cpp
@@ -4,40 +4,27 @@
 #include <string.h>
 #include <iostream>
 
-int readDentry(int directoryInodeNo, string filename, Ext2Dentry* dentry) {
-    Ext2Inode currInode;
-
-    if (!readInode(directoryInodeNo, &currInode)) {
-        printf("ReadInode Failed...\n");
-        return 0;
-    }
-
-    printInode(currInode);
-
-    char* block = new char[blockSize];
+int readDentry(Ext2Inode dirInode, string filename, Ext2Dentry* dentry) {
+    char block[blockSize];
+    memset(block, '\0', blockSize);
     int currBlockIndex = 0;
 
-    while (currBlockIndex < EXT2_NDIR_BLOCKS && currBlockIndex < currInode.i_blocks) {
-        if (!currInode.i_block[currBlockIndex])
-            break;
-
-        if (!readBlock(currInode.i_block[currBlockIndex], block)) {
-            printf("Read Block Failed...\n");
+    while ((currBlockIndex * blockSize) < dirInode.i_size) {
+        if (!readInodeBlock(dirInode, currBlockIndex, block)) {
+            printf("Read Inode Block Failed...\n");
             return 0;
         }
 
         int i = 0;
 
         while (i < blockSize) {
-            Ext2Dentry* fileDentry = (Ext2Dentry*)block;
-            // printDentry(*fileDentry);
+            Ext2Dentry* fileDentry = (Ext2Dentry*)(&block[i]);
 
             if (fileDentry->name == filename) {
                 memcpy(dentry, fileDentry, DENTRY_SIZE);
                 return 1;
             }
 
-            block += fileDentry->rec_len;
             i += fileDentry->rec_len;
         }
 
--- ext2/dentry.h
+++ ext2/dentry.h
@@ -8,7 +8,7 @@ using namespace std;
 
 #define DENTRY_SIZE sizeof(Ext2Dentry)
 
-int readDentry(int directoryInodeNo, string filename, Ext2Dentry* dentry);
+int readDentry(Ext2Inode dirInode, string filename, Ext2Dentry* dentry);
 void printDentry(Ext2Dentry dentry);
 
 #endif
