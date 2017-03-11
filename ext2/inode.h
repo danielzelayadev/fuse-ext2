a0286d2bf204f64251f68e48fc3d378cf4ec9329
wupa9 <wupaz9@gmail.com>
2017-03-11 04:46:28 -0600

    Update macros

--- ext2/inode.h
+++ ext2/inode.h
@@ -9,9 +9,15 @@ using namespace std;
 #define INODE_SIZE sizeof(Ext2Inode)
 #define ROOT_DIR_INODE 2
 
-#define IS_SI block < BLOCK_POINTERS_IN_BLOCK + EXT2_NDIR_BLOCKS
-#define IS_DI block < (BLOCK_POINTERS_IN_BLOCK * BLOCK_POINTERS_IN_BLOCK) + EXT2_NDIR_BLOCKS
-#define IS_TI block < (BLOCK_POINTERS_IN_BLOCK * BLOCK_POINTERS_IN_BLOCK * BLOCK_POINTERS_IN_BLOCK) + EXT2_NDIR_BLOCKS
+#define SI_BLOCKS BLOCK_POINTERS_IN_BLOCK
+#define DI_BLOCKS SI_BLOCKS * SI_BLOCKS
+#define TI_BLOCKS DI_BLOCKS * SI_BLOCKS
+#define SI_UL SI_BLOCKS + EXT2_NDIR_BLOCKS
+#define DI_UL DI_BLOCKS + SI_UL
+#define TI_UL TI_BLOCKS + DI_UL
+#define IS_SI (block >= EXT2_NDIR_BLOCKS && block < SI_UL)
+#define IS_DI (block >= SI_UL && block < DI_UL)
+#define IS_TI (block >= DI_UL && block < TI_UL)
 
 extern unsigned int inodesPerBlock, itableBlockCount, 
                     BLOCK_POINTERS_IN_BLOCK;
