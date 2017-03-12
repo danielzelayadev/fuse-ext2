14350b9bcd441c4aab72cbf24a79a77c5a809d28
wupa9 <wupaz9@gmail.com>
2017-03-12 17:20:38 -0600

    Implement open

--- ext2/open.cpp
+++ ext2/open.cpp
@@ -1,7 +1,18 @@
 #include "operations.h"
+#include "inode.h"
+#include <string>
+#include <errno.h>
 #include <stdio.h>
 
-int open(const char *path, struct fuse_file_info *fileInfo) {
+using namespace std;
+
+int open(const char *path, struct fuse_file_info *fi) {
     printf("OPERATION: open(path=%s)\n", path);
-    return 1;
+
+     if((fi->flags & 3) != O_RDONLY)
+        return -EACCES;
+
+    fi->fh = getInodeIndexByPath(string(path));
+
+    return 0;
 }
