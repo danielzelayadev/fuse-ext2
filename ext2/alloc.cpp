ed5f5bcfb057abdfd175d8f173820d3a18b33d76
wupa9 <wupaz9@gmail.com>
2017-03-24 03:01:02 -0600

    Update metadata in alloc block

--- ext2/alloc.cpp
+++ ext2/alloc.cpp
@@ -1,6 +1,8 @@
 #include "alloc.h"
 #include "bitmap.h"
 #include "super.h"
+#include "groupdesc.h"
+#include "../device/device.h"
 
 #include <stdio.h>
 
@@ -11,7 +13,6 @@ int getFirstFreeBlock() {
     return -1;
 }
 
-// TODO: Update Metadata
 int allocBlock() {
     int firstFreeBlock = getFirstFreeBlock();
 
@@ -20,5 +21,38 @@ int allocBlock() {
         return -1;
     }
 
-    return turnOnBit(firstFreeBlock, BLOCK_BITMAP) ? firstFreeBlock : -1;
+    if (turnOnBit(firstFreeBlock, BLOCK_BITMAP)) {
+        Ext2SuperBlock sb;
+        Ext2GroupDescriptor gd;
+        int blockGroup = BIT_BLOCK_GROUP(firstFreeBlock);
+
+        if (!readSuperBlock(&sb)) {
+            printf("allocBlock: Could not read super block.\n");
+            return -1;
+        }
+
+        sb.s_r_blocks_count++;
+        sb.s_free_blocks_count--;
+
+        if (!writeSuperBlock(&sb)) {
+            printf("allocBlock: Could not update super block.\n");
+            return -1;
+        }
+
+        if (!readGroupDesc(blockGroup, &gd)) {
+            printf("allocBlock: Could not read group desc.\n");
+            return -1;
+        }
+
+        gd.bg_free_blocks_count--;
+
+        if (!writeGroupDesc(blockGroup, &gd)) {
+            printf("allocBlock: Could not update group desc.\n");
+            return -1;
+        }
+
+        return firstFreeBlock;
+    }
+
+    return -1;
 }
