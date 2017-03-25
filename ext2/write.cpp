3148484fce13768c307a8ca33ba46d8f524e247c
wupa9 <wupaz9@gmail.com>
2017-03-25 08:10:53 -0600

    Implement write

--- ext2/write.cpp
+++ ext2/write.cpp
@@ -1,7 +1,23 @@
 #include "operations.h"
+#include "inode.h"
+#include <errno.h>
 #include <stdio.h>
 
 int write(const char* path, const char *buf, size_t size, off_t offset, struct fuse_file_info* fi) {
     printf("OPERATION: write(path=%s, size=%d, offset=%d)\n", path, size, offset);
-    return 0;
+
+    int bytesWritten = 0;
+
+    Ext2Inode inode;
+
+    printf("Path: %s === FH: %d\n", path, fi->fh);
+
+    if (!readInode(fi->fh, &inode))
+        return -EIO;
+
+    bytesWritten = writeInodeBlock(fi->fh, inode, (void*)buf, offset, size);
+
+    offset += bytesWritten;
+
+    return bytesWritten;
 }
